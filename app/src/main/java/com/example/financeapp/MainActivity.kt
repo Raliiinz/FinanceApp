cd0a9be113e877b21616586064b8c67d9cb7dff5
Raliiinz <garifullinaralina@gmail.com>
2025-06-13 17:02:45 +0300

    test2

--- app/src/main/java/com/example/financeapp/MainActivity.kt
+++ app/src/main/java/com/example/financeapp/MainActivity.kt
@@ -30,7 +30,6 @@ class MainActivity : ComponentActivity() {
                         }
                     } else {
                         MainScreen()
-                        println("nnn")
                     }
                 }
         }
